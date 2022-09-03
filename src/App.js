be3b84799c61ffa0b8a27f1302585117c9c4c2e5
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-09-02 23:34:11 -0300

    app ja com loading e menu funcionando

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import './css/styleMenu.css'
-import Tubar from  './menu-home/Tubar';
-import ListItensMenu from './menu-home/ListItensMenu'
-/*import Navegation from './menu-home/Navegation'*/
+import Tubar from './menu-home/Tubar';
+import ListItensMenu from './menu-home/ListItensMenu';
+import GridColor from './GridColor/GridColor'
+import Loading from './loanding/loanding';
+import {useState} from 'react'
 
 
 function App() {
+
+  
+  
   return (
     <div id="pp" className="App">
-      <header className="App-header">
-        <Tubar/>
-        <ListItensMenu/>
-        <h2>menu ainda não ta  </h2>
-        <h3>funcionado</h3>
-      </header>
+
+    
+
+      <Tubar />
+      <ListItensMenu />
+
+      {/* <div className='centerColor'>
+        <GridColor />
+      </div> */}
+       
+       <div className='centerColor'>
+       <Loading />
+      </div> 
+     
+
+
     </div>
   );
 }
